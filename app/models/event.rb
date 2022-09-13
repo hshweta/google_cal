7904b906f719a818a9f25f56aa0318632fc3719b
Shwetambari Helwade <shweta.helwade@gmail.com>
2022-09-13 10:40:36 +0530

    Add validations on Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  validates :title, :description, :venue, :start_date, :end_date, presence: true
+  validates_comparison_of :end_date, greater_than: :start_date
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
     # for Google OmniAuth
     :omniauthable, omniauth_providers: [:google_oauth2]
 
+  has_many :events
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.try(:info).try(:email)
